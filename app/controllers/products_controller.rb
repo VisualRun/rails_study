7b5222473d704d10b38bf6a04f78e939c717fa14
visualrun-ubuntu <wxw0813@gmail.com>
2014-12-18 23:39:30 +0800

    easy add show index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,24 @@
 class ProductsController < ApplicationController
+
+  def index
+    @products = Products.all
+  end
+
   def new
   end
   def create
     #render plain: params[:products].inspect
-    @product = Products.new(params[:products])
-    
+    @product = Products.new(products_params)
+
     @product.save
     redirect_to @product
+  end
+
+  def show
+    @products = Products.find(params[:id])
+  end
+
+  private def products_params
+    params.require(:products).permit(:title,:text)
   end
 end
