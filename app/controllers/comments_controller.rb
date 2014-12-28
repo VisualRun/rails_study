4f6037977f3ecd8180eba2845111637845a2f87c
visualrun <wxw0813@gmail.com>
2014-12-29 00:10:59 +0800

    update sm

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,16 @@
 class CommentsController < ApplicationController
+  
+  http_basic_authenticate_with name: 'wxw', password: 'secret', only: :destroy
   def create
     @product = Products.find(params[:product_id])
     @comment = @product.comments.create(comment_params)
+    redirect_to product_path(@product)
+  end
+
+  def destroy
+    @product = Products.find(params[:product_id])
+    @comment = @product.comments.find(params[:id])
+    @comment.destroy
     redirect_to product_path(@product)
   end
 
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
 
+  http_basic_authenticate_with name: "wxw",password: "secret", except: [:index, :show]
   def index
     @product = Products.all
   end
