32962a9cd3006124273373385400db0213dfc291
Eric Wang <wxw0813@gmail.com>
2015-06-18 23:58:29 +0800

    create user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141220143802) do
+ActiveRecord::Schema.define(version: 20150618154012) do
 
   create_table "comments", force: true do |t|
     t.string   "commenter"
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 20141220143802) do
   create_table "products", force: true do |t|
     t.string   "title"
     t.text     "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
