c38bab5e029c07290ec3550c93c8da15efe86ef0
Eric Wang <wxw0813@gmail.com>
2015-06-22 23:32:06 +0800

    add mircopost

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150618154012) do
+ActiveRecord::Schema.define(version: 20150622153044) do
 
   create_table "comments", force: true do |t|
     t.string   "commenter"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 20150618154012) do
   end
 
   add_index "comments", ["products_id"], name: "index_comments_on_products_id"
+
+  create_table "microposts", force: true do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "products", force: true do |t|
     t.string   "title"
