5d0122627667d00cb4f32eaf90c4ecf3380b99f4
VisualRun <visualrun@visualrun-rmbp15.local>
2015-11-17 17:59:07 +0800

    edit layout && import bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
 
-  get 'static_pages/help'
+  get 'help' => 'static_pages#help'
 
-  get 'static_pages/about'
+  get 'about' => 'static_pages#about'
+
+  get 'contact' => 'static_pages#contact'
 
   resources :microposts
 
