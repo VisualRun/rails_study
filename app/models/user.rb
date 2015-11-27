03509fd8285201d51cd862fbdb00133816907ef8
VisualRun <wxw0813@gmail.com>
2015-11-27 09:04:20 +0800

    edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   VALID_EMAILL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAILL_REGEX}, uniqueness: { case_sensitive: false }
 
-  before_save { self.email = email.downcase }
+  before_save { email.downcase! }
 
   has_secure_password
 
